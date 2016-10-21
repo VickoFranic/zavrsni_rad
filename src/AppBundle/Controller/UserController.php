630573bf26b08eac82d9dba767f6523c7cd3b646
avuletic <vul4.ljuska@gmail.com>
2016-10-21 23:49:30 +0200

    Popravio sam anotaciju @User u controlleru

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,6 +3,10 @@
 namespace AppBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Entity\User;
 use AppBundle\Form\UserType;
 
